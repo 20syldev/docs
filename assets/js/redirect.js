1971888e0a60296683c33d1a51710293bd959141
20syldev <git@sylvain.pro>
2025-01-31 23:44:15 +0100

    MEDIUM: js: Updated redirect.js with sub-pages (/v.../page/page)

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,3 +1,1 @@
-$(document).ready(function() {
-    $(() => /^\/v\d+\/[^/]+$/.test(location.pathname) && location.replace('/' + (navigator.language.startsWith('en') ? 'en' : 'fr') + location.pathname));
-});
+$(document).ready(() => /^\/v\d+\/[^/]+(?:\/[^/]+)?$/.test(location.pathname) && location.replace('/' + (navigator.language.startsWith('en') ? 'en' : 'fr') + location.pathname));
