7a412c566ecab700018d0ca72bf35549fde90e20
20syldev <git@sylvain.pro>
2026-01-12 11:49:08 +0100

    MEDIUM: app: Removed unused components in configuration

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -2,19 +2,13 @@ import DefaultTheme from 'vitepress/theme';
 import type { Theme } from 'vitepress';
 import './main.css';
 
-import Method from './components/Method.vue';
 import Endpoint from './components/Endpoint.vue';
-import Request from './components/Request.vue';
-import Terminal from './components/Terminal.vue';
 import Layout from './components/Layout.vue';
 
 export default {
     extends: DefaultTheme,
     Layout,
     enhanceApp({ app }) {
-        app.component('HBadge', Method);
-        app.component('HEndpoint', Endpoint);
-        app.component('HRequest', Request);
-        app.component('HTerminal', Terminal);
+        app.component('Endpoint', Endpoint);
     },
 } satisfies Theme;
