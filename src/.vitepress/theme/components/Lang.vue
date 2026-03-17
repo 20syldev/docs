7cd90ea69102761e6e0405bcc5cfe87b0e1c9cb1
20syldev <git@sylvain.sh>
2026-03-18 00:30:10 +0100

    MEDIUM: components: Simplified Lang component

--- src/.vitepress/theme/components/Lang.vue
+++ src/.vitepress/theme/components/Lang.vue
@@ -2,18 +2,16 @@
 import { computed } from 'vue';
 import { useRoute } from 'vitepress';
 import VPFlyout from 'vitepress/dist/client/theme-default/components/VPFlyout.vue';
-import { getLang, isHome, getLangItems } from '../i18n';
+import { getLang, isHome, getLangItems, t } from '../i18n';
 
 const route = useRoute();
 
 const homepage = computed(() => isHome(route.path));
 
-const lang = computed(() => {
-    const l = getLang(route.path);
-    return l === 'fr' ? 'FR' : 'EN';
-});
+const lang = computed(() => getLang(route.path).toUpperCase());
 
-const items = computed(() => getLangItems(route.path, homepage.value));
+const langCode = computed(() => getLang(route.path));
+const items = computed(() => getLangItems(route.path, homepage.value, langCode.value));
 
 const visible = computed(() => route.path.includes('/v') || homepage.value);
 </script>
