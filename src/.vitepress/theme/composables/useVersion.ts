938546674715c8d4b4fad95ec74eedb1add6cd69
20syldev <git@sylvain.sh>
2026-04-08 01:09:29 +0200

    MEDIUM: composables: Added useVersion composable for version-aware paths

---
+++ src/.vitepress/theme/composables/useVersion.ts
@@ -0,0 +1,19 @@
+import { computed } from 'vue';
+import { useRoute } from 'vitepress';
+import { LATEST_VERSION } from '../utils/redirect';
+import { getLang } from '../utils/i18n';
+
+export function useVersion() {
+    const route = useRoute();
+
+    const version = computed(() =>
+        route.path.match(/^\/(v\d+)\//)?.[1] ?? LATEST_VERSION
+    );
+
+    const lang = computed(() => getLang(route.path));
+
+    const versionedPath = (path: string) =>
+        `/${version.value}${path.startsWith('/') ? path : '/' + path}`;
+
+    return { version, lang, versionedPath };
+}
