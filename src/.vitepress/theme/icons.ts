144dfe6b3c5b1cde71407a7ab045e8238176a8fa
20syldev <git@sylvain.sh>
2026-04-08 00:48:01 +0200

    MEDIUM: utils: Move i18n, icons and redirect to dedicated subdirectory

--- src/.vitepress/theme/icons.ts
+++
@@ -1,3 +0,0 @@
-export const copyIcon = `<svg class="copy" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect class="copy-front" width="14" height="14" x="8" y="8" rx="2" ry="2"/><path class="copy-back" d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>`;
-
-export const checkIcon = `<svg class="check" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6 9 17l-5-5"/></svg>`;
