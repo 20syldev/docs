ee627ed6fb99bf7ac31d353cf108ba3debce517e
20syldev <git@sylvain.sh>
2026-04-13 16:56:11 +0200

    BUILD: Add simple-import-sort plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import vue from 'eslint-plugin-vue';
-import vueParser from 'vue-eslint-parser';
 import prettier from 'eslint-config-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import vue from 'eslint-plugin-vue';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import vueParser from 'vue-eslint-parser';
 
 export default [
     {
@@ -20,12 +21,17 @@ export default [
         },
     },
     {
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
         languageOptions: {
             globals: { ...globals.browser },
         },
         rules: {
             'vue/multi-word-component-names': 'off',
             'vue/no-v-html': 'off',
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
         },
     },
     {
