adaff63eba8b5bc56f5bffddb5986a20896a0d3f
20syldev <git@sylvain.sh>
2026-04-13 20:46:27 +0200

    MINOR: components: Add sm variant to Method badge

--- src/.vitepress/theme/components/Method.vue
+++ src/.vitepress/theme/components/Method.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-defineProps<{
-    type: 'get' | 'post' | 'put' | 'delete' | 'patch';
-}>();
+withDefaults(
+    defineProps<{
+        type: 'get' | 'post' | 'put' | 'delete' | 'patch';
+        sm?: boolean;
+    }>(),
+    { sm: false },
+);
 </script>
 
 <template>
-    <span class="method-badge" :class="type">
+    <span class="method-badge" :class="[type, { sm }]">
         {{ type.toUpperCase() }}
     </span>
 </template>
