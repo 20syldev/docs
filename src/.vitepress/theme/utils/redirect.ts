9de496fd2a30b00b441196f25d3f67073793ef00
20syldev <git@sylvain.sh>
2026-05-26 00:43:43 +0200

    MEDIUM: utils: Bump LATEST_VERSION to v5

--- src/.vitepress/theme/utils/redirect.ts
+++ src/.vitepress/theme/utils/redirect.ts
@@ -1,8 +1,8 @@
 import { defaultLang, langCodes, languages } from './i18n';
 
 export const API_BASE_URL = 'https://api.sylvain.sh';
-export const LATEST_VERSION = 'v4';
-export const KNOWN_VERSIONS = ['v1', 'v2', 'v3', 'v4'];
+export const LATEST_VERSION = 'v5';
+export const KNOWN_VERSIONS = ['v1', 'v2', 'v3', 'v4', 'v5'];
 
 const versionNums = KNOWN_VERSIONS.map((v) => v.replace('v', '')).join('');
 export const VERSION_REGEX = new RegExp(`^v[${versionNums}]$`);
