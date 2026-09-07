3e0a4fa6b9ed6d5fd0499f25c0a66aafebc2f24b
20syldev <git@sylvain.sh>
2026-09-07 14:32:25 +0200

    MEDIUM: sidebar: Add 5.7.0 changelog entry

--- src/.vitepress/sidebar/v5.ts
+++ src/.vitepress/sidebar/v5.ts
@@ -114,6 +114,7 @@ export const v5 = {
                     collapsed: true,
                     items: [
                         { text: 'Overview', link: '/v5/en/changelog' },
+                        { text: '5.7.0', link: '/v5/en/changelog/5.7.0' },
                         { text: '5.6.0', link: '/v5/en/changelog/5.6.0' },
                         { text: '5.5.0', link: '/v5/en/changelog/5.5.0' },
                         { text: '5.4.0', link: '/v5/en/changelog/5.4.0' },
@@ -244,6 +245,7 @@ export const v5 = {
                     collapsed: true,
                     items: [
                         { text: 'Aperçu', link: '/v5/fr/changelog' },
+                        { text: '5.7.0', link: '/v5/fr/changelog/5.7.0' },
                         { text: '5.6.0', link: '/v5/fr/changelog/5.6.0' },
                         { text: '5.5.0', link: '/v5/fr/changelog/5.5.0' },
                         { text: '5.4.0', link: '/v5/fr/changelog/5.4.0' },
